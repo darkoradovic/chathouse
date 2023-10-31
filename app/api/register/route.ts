a3e6c71332b0209712c90084d0eaccfab62ba51e
darkoradovic <darmior5@gmail.com>
2023-10-31 08:35:04 +0100

    Feat: authentication options

---
+++ app/api/register/route.ts
@@ -0,0 +1,38 @@
+import { hash } from "bcrypt";
+import prisma from '@/app/libs/prismadb'
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, password } = (await req.json()) as {
+      name: string;
+      email: string;
+      password: string;
+    };
+    const hashed_password = await hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email: email.toLowerCase(),
+        hashedPassword: `${hashed_password}`
+      },
+    });
+
+    return NextResponse.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        password: user.hashedPassword
+      },
+    });
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        status: "error",
+        message: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
