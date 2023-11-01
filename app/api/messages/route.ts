7b0abe00cd484900899c15f89915f815e3f64779
darkoradovic <darmior5@gmail.com>
2023-11-01 12:17:36 +0100

    Feat: create message and image upload

---
+++ app/api/messages/route.ts
@@ -0,0 +1,62 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await req.json();
+    const { message, image, conversationId } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma?.message.create({
+      include: {
+        seen: true,
+        sender: true,
+      },
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: { id: conversationId },
+        },
+        sender: {
+          connect: { id: currentUser.id },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+      },
+    });
+
+    const updatedConversation = await prisma?.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage?.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error) {
+    return NextResponse.json("An error ocured.", { status: 500 });
+  }
+};
